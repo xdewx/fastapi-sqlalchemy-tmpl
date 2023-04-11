2bc3144db60a855799287a5c503f1c7471649348
leoking <present150608@sina.com>
2023-04-12 01:58:21 +0800

    test(orm): 测试fastapi_sqlalchemy的用法

--- routers/test.py
+++ routers/test.py
@@ -4,7 +4,7 @@
 #
 # Author: leoking
 # Date: 2023-04-12 01:48:31
-# LastEditTime: 2023-04-12 01:49:36
+# LastEditTime: 2023-04-12 01:56:52
 # LastEditors: leoking
 # Description:
 #
@@ -12,7 +12,9 @@ from fastapi import APIRouter
 from fastapi_sqlalchemy import (
     db,
 )
-from sqlalchemy import text  # an object to provide global access to a database session
+from sqlalchemy import text
+
+from models import TestTable  # an object to provide global access to a database session
 
 router = APIRouter()
 
@@ -22,7 +24,7 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@router.get("/test/db")
+@router.get("/db/write")
 async def test_db():
     from models import TestTable
 
@@ -38,3 +40,29 @@ async def test_db():
     )
     db.session.add(TestTable(name="xxxxxx"))
     return db.session.query(TestTable).all()
+
+
+@router.get("/db/read/1")
+async def read(id: int):
+    item = db.session.query(TestTable).get(id)
+    return item
+
+
+@router.get("/db/read/failed")
+def read_failed(id: int):
+    item = db.session.query(TestTable).get(id)
+    return item
+
+
+@router.get("/db/read/3")
+def read_2(id: int):
+    with db():
+        item = db.session.query(TestTable).get(id)
+    return item
+
+
+@router.get("/db/read/4")
+def read_3(id: int):
+    item = db.session.query(TestTable).get(id)
+    db.session.close()
+    return item
